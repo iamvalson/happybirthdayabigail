c0c136e6000f098d63641d53cae02d259a70b8f3
iamvalson <148555333+iamvalson@users.noreply.github.com>
2024-11-18 15:28:25 +0100

    Update happy birtday.js

--- happy birtday.js
+++ happy birtday.js
@@ -5,7 +5,7 @@ let w = (c.width = window.innerWidth),
 (hh = h / 2),
   (opts = {
     // change the text in here //
-    strings: ["HAPPY", "BIRTHDAY!", "Noelle"],
+    strings: ["HAPPY", "BIRTHDAY!", "Abigail Williams", "aka Noelle", "aka Mummy Scotland"],
     charSize: 30,
     charSpacing: 35,
     lineHeight: 40,
